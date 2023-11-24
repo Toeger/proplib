2f7e7322cf2911fb4dee934b8a146bda1b5ef944
toeger <toeger@anony.mous>
2023-11-25 00:21:43 +0100

    Simplified example

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,11 +1,8 @@
 #include "../proplib/ui/label.h"
 #include "../proplib/ui/window.h"
 
-int main(int argc, char *argv[]) {
+int main() {
 	prop::Window w{"Prop Demo"};
-	auto label = std::make_unique<prop::Label>("Hello world!");
-	label->x = 100;
-	label->y = 200;
-	w.widget = std::move(label);
+	w.widget = prop::Label{"Hello world!"};
 	w.exec();
 }
