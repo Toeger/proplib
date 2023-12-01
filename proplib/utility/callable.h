56d9c297b1d241c909b1447c8aea2d628c9e190a
toeger <toeger@anony.mous>
2023-12-01 23:46:14 +0100

    Added callable and typelist helpers

---
+++ proplib/utility/callable.h
@@ -0,0 +1,108 @@
+#include <type_traits>
+#include <utility>
+
+#include "type_list.h"
+
+namespace prop {
+	//helpers to extract callable information
+	namespace detail {
+		//return type
+		template <class Return_type, class... Args>
+		auto get_return_type(Return_type (*)(Args...)) -> Return_type;
+		template <class Return_type, class Class_type, class... Args>
+		auto get_return_type(Return_type (Class_type::*)(Args...)) -> Return_type;
+		template <class Return_type, class Class_type, class... Args>
+		auto get_return_type(Return_type (Class_type::*)(Args...) const) -> Return_type;
+		template <class T>
+		auto get_return_type(T &&) -> decltype(get_return_type(&std::remove_reference_t<T>::operator()));
+		template <class T>
+		using Return_type = decltype(get_return_type(std::declval<T>()));
+
+		//class type
+		template <class Return_type, class Class_type, class... Args>
+		auto get_class_type(Return_type (Class_type::*)(Args...)) -> Class_type;
+		template <class Return_type, class Class_type, class... Args>
+		auto get_class_type(Return_type (Class_type::*)(Args...) const) -> Class_type;
+		template <class T>
+		auto get_class_type(T &&) -> decltype(get_class_type(&std::remove_reference_t<T>::operator()));
+		template <class T>
+		using Class_type = decltype(get_class_type(std::declval<T>()));
+		template <class T>
+		auto get_has_class_type(T &&t) -> decltype(get_class_type(std::forward<T>(t)), std::true_type{});
+		std::false_type get_has_class_type(...);
+		template <class T>
+		constexpr bool has_class_type = decltype(get_has_class_type(std::declval<T>()))::value;
+
+		//arguments
+		template <class Return_type, class... Args>
+		auto get_args(Return_type (*)(Args...)) -> Type_list<Args...>;
+		template <class Return_type, class Class_type, class... Args>
+		auto get_args(Return_type (Class_type::*)(Args...)) -> Type_list<Args...>;
+		template <class Return_type, class Class_type, class... Args>
+		auto get_args(Return_type (Class_type::*)(Args...) const) -> Type_list<Args...>;
+		template <class T>
+		auto get_args(T &&) -> decltype(get_args(&std::remove_reference_t<T>::operator()));
+		template <class T>
+		using Args = decltype(get_args(std::declval<T>()));
+
+		//get function pointer
+		template <class Return_type, class... Args>
+		auto get_function_pointer(Type_list<Args...>) -> Return_type (*)(Args...);
+		template <class Return_type, class Class, class... Args>
+		auto get_member_function_pointer(Type_list<Args...>) -> Return_type (Class::*)(Args...);
+		template <class Return_type, class Class, class... Args>
+		auto get_const_member_function_pointer(Type_list<Args...>) -> Return_type (Class::*)(Args...) const;
+	} // namespace detail
+
+	//struct to hold callable information
+	template <class T, bool has_class>
+	struct Callable_info;
+
+	template <class T>
+	struct Callable_info<T, true> {
+		Callable_info(T &&) {}
+		Callable_info(const T &) {}
+		Callable_info() = default;
+		using Return_type = detail::Return_type<T>;
+		using Class_type = detail::Class_type<T>;
+		constexpr static bool has_class_type = true;
+		using Args = detail::Args<T>;
+		using as_function_pointer = decltype(detail::get_member_function_pointer<Return_type, Class_type>(Args{}));
+		using as_non_member_function_pointer = decltype(detail::get_function_pointer<Return_type>(Args{}));
+	};
+
+	template <class T>
+	struct Callable_info<T, false> {
+		Callable_info(T &&) {}
+		Callable_info(const T &) {}
+		Callable_info() = default;
+		using Return_type = detail::Return_type<T>;
+		constexpr static bool has_class_type = false;
+		using Args = detail::Args<T>;
+		using as_function_pointer = decltype(detail::get_function_pointer<Return_type>(Args{}));
+		using as_non_member_function_pointer = decltype(detail::get_function_pointer<Return_type>(Args{}));
+	};
+
+	template <class T>
+	Callable_info(T &&) -> Callable_info<std::remove_reference_t<T>, detail::has_class_type<T>>;
+
+	template <class T>
+	using Callable_info_for = decltype(Callable_info(std::declval<T>()));
+
+	//helper to make a function pointer
+	template <class Return_type, class Typelist>
+	using function_pointer = decltype(detail::get_function_pointer<Return_type>(Typelist{}));
+	template <class Return_type, class Class, class Typelist>
+	using member_function_pointer = decltype(detail::get_member_function_pointer<Return_type, Class>(Typelist{}));
+	template <class Return_type, class Class, class Typelist>
+	using const_member_function_pointer =
+		decltype(detail::get_const_member_function_pointer<Return_type, Class>(Typelist{}));
+
+	//make overloaded functions
+	template <class... Callables>
+	struct Overload : Callables... {
+		Overload(Callables &&...callables)
+			: Callables(std::forward<Callables>(callables))... {}
+		using Callables::operator()...;
+	};
+} // namespace prop
---
+++ proplib/utility/type_list.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+namespace prop {
+	//store and manipulate types
+	template <class... Ts>
+	struct Type_list {
+		private:
+		//helper to get type at index
+		template <int index, class... Args>
+		struct Get_at;
+		template <class T, class... Args>
+		struct Get_at<0, T, Args...> {
+			using Type = T;
+		};
+		template <int index>
+		struct Get_at<index> {};
+		template <int index, class T, class... Args>
+		struct Get_at<index, T, Args...> {
+			using Type = typename Get_at<index - 1, Args...>::Type;
+		};
+		//helper to remove a type
+		template <class T, class... Args>
+		struct Remove;
+		template <class T>
+		struct Remove<T> {
+			using types = Type_list<>;
+		};
+		template <class T, class Head, class... Tail>
+		struct Remove<T, Head, Tail...> {
+			using types = std::conditional_t<std::is_same_v<T, Head>, typename Remove<T, Tail...>::types,
+											 typename Remove<T, Tail...>::types::template prepend<Head>>;
+		};
+		//helper for contains
+		template <class T, class... Args>
+		struct Contains;
+		template <class T, class First, class... Args>
+		struct Contains<T, First, Args...> {
+			static constexpr bool value = std::is_same_v<T, First> || Contains<T, Args...>::value;
+		};
+		template <class T, class... Args>
+		struct Contains {
+			static constexpr bool value = false;
+		};
+		template <class... Args>
+		static auto concat(Type_list<Args...>) -> Type_list<Ts..., Args...>;
+
+		public:
+		template <class... T>
+		using append = Type_list<Ts..., T...>;
+		template <class... T>
+		using prepend = Type_list<T..., Ts...>;
+		template <template <typename...> class F>
+		using apply = Type_list<F<Ts>...>;
+		template <template <typename...> class T>
+		using instantiate = T<Ts...>;
+		template <int index>
+		using at = typename Get_at < index<0 ? sizeof...(Ts) + index : index, Ts...>::Type;
+		template <class T>
+		using concatenate = decltype(concat(T{}));
+		template <class T>
+		using remove = typename Remove<T, Ts...>::types;
+		template <class T>
+		constexpr static bool contains_v = Contains<T, Ts...>::value;
+		constexpr static std::size_t size = sizeof...(Ts);
+		using index_sequence = std::index_sequence_for<Ts...>;
+
+		//TODO: sort, remove_if
+	};
+	namespace detail {
+		template <template <typename...> class T, class... Args>
+		auto adopt_from(T<Args...> &&) -> Type_list<Args...>;
+	}
+	template <class T>
+	using adopt_from = decltype(detail::adopt_from(std::declval<T>()));
+} // namespace prop
