196f446e7680b67ee664303e875f7c9a18ff376c
toeger <toeger@anony.mous>
2023-12-01 23:50:21 +0100

    Exception for severed explicit dependencies

--- proplib/utility/exceptions.h
+++ proplib/utility/exceptions.h
@@ -3,11 +3,18 @@
 #include <stdexcept>
 
 namespace prop {
+	//thrown when a Polywrap<CopyableBase> is being assigned a NonCopyableDerived and then the Polywrap<CopyableBase> is being copied
 	struct Copy_error : std::runtime_error {
 		using std::runtime_error::runtime_error;
 	};
 
+	//thrown on internal logic errors that should never happen
 	struct Logic_error : std::logic_error {
 		using std::logic_error::logic_error;
+	};
+
+	//thrown when a required explicit dependency is unavailable, caught internally
+	struct Property_expired : std::runtime_error {
+		using std::runtime_error::runtime_error;
 	};
 } // namespace prop
